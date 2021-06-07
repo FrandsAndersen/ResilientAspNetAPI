eac5f35b6c18cff1a6608ed6db06a2a27a8def0b
fan <fan@ditmer.dk>
2021-06-07 16:01:00 +0200

    - Added timout policy

--- ProjectA/Startup.cs
+++ ProjectA/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Polly;
 using Polly.Extensions.Http;
+using Polly.Retry;
+using Polly.Timeout;
+using Polly.Wrap;
 using ProjectA.Services;
 using System;
 using System.Collections.Generic;
@@ -37,7 +40,7 @@ namespace ProjectA
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectA", Version = "v1" });
             });
-            services.AddHttpClient("MyNamedClient", client =>
+            services.AddHttpClient("MyNamedClient", client => // Named Client : DELETE THIS COMMENT
             {
                 client.BaseAddress = new Uri("https://localhost:44383/api/ProjectB");
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
@@ -47,21 +50,24 @@ namespace ProjectA
                 TimeSpan.FromSeconds(3),
             }));
 
-            services.AddHttpClient<IRequestStringService, RequestStringService>(client =>
+            services.AddHttpClient<IRequestStringService, RequestStringService>(client => // Typed Client : DELETE THIS COMMENT
             {
                 client.BaseAddress = new Uri("https://localhost:44383/api/ProjectB");
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
             })
-                .SetHandlerLifetime(TimeSpan.FromMinutes(5)) // Set life time to five minutes
-                .AddPolicyHandler(GetRetryPolicy());
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5)) // Set life time to five minutes,
+                                                             //.AddPolicyHandler(GetRetryPolicy());
+                .AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
         }
 
         private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
         {
             Random jitterer = new Random();
             return HttpPolicyExtensions
-                 .HandleTransientHttpError()
-                 .OrResult(res => !res.IsSuccessStatusCode)
+                 .HandleTransientHttpError() // TODO: Find out what this does...
+                 .OrResult(res => !res.IsSuccessStatusCode) // Retry if status code != 200
+                 .Or<TimeoutRejectedException>() // Retry when TimeoutRejectedException are thrown
                  .WaitAndRetryAsync(
                       4,
                      retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
@@ -75,6 +81,17 @@ namespace ProjectA
                      });
 
         }
+
+        //private static IAsyncPolicy<HttpResponseMessage> GetTimeoutPolicy()
+        //{
+        //    AsyncPolicy timeoutPolicy = Policy.TimeoutAsync(30);
+        //    HttpResponseMessage httpResponse = timeoutPolicy
+        //        .ExecuteAsync(
+        //          async ct => await httpClient.GetAsync(endpoint, ct), // Execute a delegate which responds to a CancellationToken input parameter.
+        //          CancellationToken.None // In this case, CancellationToken.None is passed into the execution, indicating you have no independent cancellation control you wish to add to the cancellation provided by TimeoutPolicy.  Your own indepdent CancellationToken can also be passed - see wiki for examples.
+        //          );
+
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
